d766d0c4cd863a7e5370f40abe0ef24129cc4edc
Bruno Parga <brunoparga@gmail.com>
2018-04-10 14:56:29 -0300

    Delete routes added by rails generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,4 @@
 Rails.application.routes.draw do
-  get 'sold_tickets/index'
-
-  get 'sold_tickets/show'
-
-  get 'sold_tickets/new'
-
-  get 'screenings/index'
-
-  get 'screenings/show'
-
-  get 'screenings/new'
-
-  get 'screenings/create'
-
-  get 'screenings/destroy'
-
-  get 'screens/index'
-
-  get 'screens/show'
-
-  get 'formats_screens/index'
-
-  get 'formats_screens/new'
-
-  get 'formats_screens/create'
-
-  get 'formats_screens/update'
-
-  get 'formats/edit'
-
-  get 'formats/update'
-
   devise_for :users
   root to: 'pages#home'
 
