75d47757c7250e36c880c89bf6fa3b7718ba1deb
Rodrigo Arroyo <rjr.arroyo@gmail.com>
2018-04-10 15:53:14 -0300

    Add admin and owner booleans to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410142850) do
+ActiveRecord::Schema.define(version: 20180410184924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(version: 20180410142850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "address", null: false
+    t.boolean "admin", default: false, null: false
+    t.boolean "theater_owner", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
