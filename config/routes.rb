43b35e0dbcba20bd537a95c858189db746612ec1
Bruno Parga <brunoparga@gmail.com>
2018-04-10 14:47:51 -0300

    Rails-generate controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,51 @@
 Rails.application.routes.draw do
+  get 'sold_tickets/index'
+
+  get 'sold_tickets/show'
+
+  get 'sold_tickets/new'
+
+  get 'screenings/index'
+
+  get 'screenings/show'
+
+  get 'screenings/new'
+
+  get 'screenings/create'
+
+  get 'screenings/destroy'
+
+  get 'screens/index'
+
+  get 'screens/show'
+
+  get 'formats_screens/index'
+
+  get 'formats_screens/new'
+
+  get 'formats_screens/create'
+
+  get 'formats_screens/update'
+
+  get 'formats/edit'
+
+  get 'formats/update'
+
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :film_titles do
+    resources :detailed_films, only: %i[new create]
+  end
+  resources :detailed_films, except: %i[new create]
+
+  resources :formats, only: %i[edit update]
+  resources :screens, except: %[update] do
+    resources :formats_screens, only: :index
+    resources :screenings do
+      resources :sold_tickets
+    end
+  end
+
+
 end
