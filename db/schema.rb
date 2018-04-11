04ff9e3f855e281262fc1217583c72a776cb2319
fabricio zanette <fabzanette@gmail.com>
2018-04-11 11:05:16 -0300

    Solve differences

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 20180410184924) do
   enable_extension "plpgsql"
 
   create_table "detailed_films", force: :cascade do |t|
-    t.integer "speech"
-    t.bigint "film_title_id"
-    t.bigint "format_id"
+    t.integer "speech", null: false
+    t.bigint "film_title_id", null: false
+    t.bigint "format_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["film_title_id", "speech", "format_id"], name: "index_detailed_films_on_film_title_id_and_speech_and_format_id", unique: true
@@ -27,51 +27,55 @@ ActiveRecord::Schema.define(version: 20180410184924) do
   end
 
   create_table "film_titles", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "imdb_id", null: false
   end
 
   create_table "formats", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "format_uniqueness_index", unique: true
   end
 
   create_table "formats_screens", force: :cascade do |t|
-    t.bigint "format_id"
-    t.bigint "screen_id"
+    t.bigint "format_id", null: false
+    t.bigint "screen_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["format_id", "screen_id"], name: "index_formats_screens_on_format_id_and_screen_id", unique: true
     t.index ["format_id"], name: "index_formats_screens_on_format_id"
     t.index ["screen_id"], name: "index_formats_screens_on_screen_id"
   end
 
   create_table "screenings", force: :cascade do |t|
-    t.integer "initial_tickets"
+    t.integer "initial_tickets", null: false
     t.datetime "session_time"
     t.datetime "closing_time"
-    t.bigint "detailed_film_id"
-    t.bigint "screen_id"
+    t.bigint "detailed_film_id", null: false
+    t.bigint "screen_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["detailed_film_id"], name: "index_screenings_on_detailed_film_id"
     t.index ["screen_id"], name: "index_screenings_on_screen_id"
+    t.index ["session_time", "detailed_film_id", "screen_id"], name: "film_screen_time_uniqueness_index", unique: true
   end
 
   create_table "screens", force: :cascade do |t|
-    t.integer "capacity"
+    t.integer "capacity", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "address", null: false
+    t.string "name", null: false
+    t.index ["name"], name: "screens_uniqueness_index", unique: true
   end
 
   create_table "sold_tickets", force: :cascade do |t|
-    t.integer "price"
     t.datetime "purchase_time"
-    t.bigint "screening_id"
-    t.bigint "user_id"
+    t.bigint "screening_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_centavos", default: 0, null: false
@@ -92,7 +96,7 @@ ActiveRecord::Schema.define(version: 20180410184924) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "address"
+    t.text "address", null: false
     t.boolean "admin", default: false, null: false
     t.boolean "theater_owner", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
