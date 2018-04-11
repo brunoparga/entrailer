8e1ac70c77ce6cec5c2d00b0f6e2d19dd28ab416
Rodrigo Arroyo <rjr.arroyo@gmail.com>
2018-04-11 12:18:42 -0300

    New policies, migrations and views

--- app/controllers/detailed_films_controller.rb
+++ app/controllers/detailed_films_controller.rb
@@ -2,15 +2,21 @@ class DetailedFilmsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
   def index
+    @detailed_films = DetailedFilm.all
+    authorize @detailed_films
   end
 
   def show
+    @detailed_film = DetailedFilm.find(params[:id])
+    authorize @detailed_film
   end
 
   def new
+    @detailed_film = DetailedFilm.new
   end
 
   def create
+
   end
 
   def edit
