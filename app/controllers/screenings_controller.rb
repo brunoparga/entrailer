7b7fe51137fadd3f38cf13ada98f4a48a01297f6
Rodrigo Arroyo <rjr.arroyo@gmail.com>
2018-04-11 09:34:46 -0300

    Add address field to sign-up form

--- app/controllers/screenings_controller.rb
+++ app/controllers/screenings_controller.rb
@@ -2,17 +2,35 @@ class ScreeningsController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index, :show]
 
   def index
+    @screenings = Screening.all
   end
 
   def show
+    @screening = Screening.find(params[:id])
   end
 
   def new
+    @screening = Screening.new
   end
 
   def create
+    @screening = screening.new(params_screening)
+    authorize @screening
+    if @screening.save
+      redirect_to screen_screening_path(@screening)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def params_screening
+    params.require(:screening).permit(:initial_tickets,
+                                      :detailed_film_id,
+                                      :screen_id, :session_time, :closing_time)
   end
 end
