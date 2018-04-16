6a5549a9e5d9a8b7cd8be11a3cae3a2110ef5f03
Bruno Parga <brunoparga@gmail.com>
2018-04-16 15:43:01 -0300

    Set letter_opener as the mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: "http://entrailer.herokuapp.com" }
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
 
