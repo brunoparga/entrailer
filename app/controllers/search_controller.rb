1d329bac7aae1339cc371ce17697a0b5e794254e
Bruno Parga <brunoparga@gmail.com>
2018-04-17 14:56:16 -0300

    Improve search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,27 +1,6 @@
 class SearchController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def old_index
-
-    if Screen.search_by_name("#{params[:query]}") != []
-      @screens = Screen.search_by_name("#{params[:query]}")
-    else
-      @screens = policy_scope(Screen)
-    end
-
-    if FilmTitle.search_by_title("#{params[:query]}") != []
-      @film_titles = policy_scope(FilmTitle.search_by_title("#{params[:query]}"))
-    else
-      @film_titles = policy_scope(FilmTitle)
-    end
-
-
-    if Screening.search_by_session_time("#{params[:query]}") != []
-      @screenings = policy_scope(Screening.search_by_session_time("#{params[:query]}"))
-    end
-    set_markers(@screens)
-  end
-
   def index
     # @screenings is an array of *screenings*, iterated on in the view.
     @screenings = []
@@ -49,7 +28,6 @@ class SearchController < ApplicationController
 
     @screens = @screenings.map { |screening| screening.screen }
     set_markers(@screens)
-    authorize @screenings
   end
 
   private
