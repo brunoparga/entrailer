5d110a5b7a9595407daa40ab24e36885dc08821f
Bruno Parga <brunoparga@gmail.com>
2018-04-17 14:41:31 -0300

    Break search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,40 +1,62 @@
 class SearchController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def index
+  def old_index
 
     if Screen.search_by_name("#{params[:query]}") != []
       @screens = Screen.search_by_name("#{params[:query]}")
-      set_markers(@screens)
     else
       @screens = policy_scope(Screen)
-      set_markers(@screens)
     end
 
     if FilmTitle.search_by_title("#{params[:query]}") != []
       @film_titles = policy_scope(FilmTitle.search_by_title("#{params[:query]}"))
-       set_markers(@screens)
     else
       @film_titles = policy_scope(FilmTitle)
-      set_markers(@screens)
     end
 
 
     if Screening.search_by_session_time("#{params[:query]}") != []
       @screenings = policy_scope(Screening.search_by_session_time("#{params[:query]}"))
-        set_markers(@screens)
     end
+    set_markers(@screens)
+  end
+
+  def index
+    # @screenings is an array of *screenings*, iterated on in the view.
+    @screenings = []
+
+    # Add all screenings in all screens that match the query
+    Screen.search_by_name("#{params[:query]}").each do |screen|
+      Screening.where(screen: screen).each do |screening|
+        @screenings << screening
+      end
+    end
+
+    # Add all screenings whose title matches the query
+    FilmTitle.search_by_title("#{params[:query]}").each do |film_title|
+      DetailedFilm.where(film_title: film_title).each do |detailed_film|
+        Screening.where(detailed_film: detailed_film).each  do |screening|
+          @screenings << screening
+        end
+      end
+    end
+
+    # Add all screenings whose time matches the query
+    Screening.search_by_session_time("#{params[:query]}").each do |screening|
+      @screenings << screening
+    end
+
+    @screens = @screenings.map { |screening| screening.screen }
+    set_markers(@screens)
+    authorize @screenings
   end
 
   private
 
   def set_markers(screens)
-      @markers = screens.map do |flat|
-      {
-        lat: flat.latitude,
-        lng: flat.longitude#,
-        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
-      }
+      @markers = screens.map do |screen|
+        { lat: screen.latitude, lng: screen.longitude }
     end
   end
 end
